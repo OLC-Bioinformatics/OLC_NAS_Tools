b89fa1731e95ee638b532fd05a9040ba576177b9
forestdussault <forest.dussault@inspection.gc.ca>
2018-04-13 14:49:00 -0400

    Docstring update

--- nastools.py
+++ nastools.py
@@ -46,10 +46,11 @@ def verify_folders():
 
 def retrieve_nas_files(seqids, outdir, copyflag, filetype, verbose_flag=False):
     """
-    :param seqids: list containing valid OLC Seq IDs
-    :param outdir: path to directory to dump requested files
-    :param copyflag: boolean flag to determine with to copy files to create symlinks
-    :param filetype: accepts string of either 'fastq' or 'fasta' to determine where to search for files
+    :param seqids: LIST containing strings of valid OLC Seq IDs
+    :param outdir: STRING path to directory to dump requested files
+    :param copyflag: BOOL flag to determine with to copy files to create symlinks
+    :param filetype: STRING of either 'fastq' or 'fasta' to determine where to search for files
+    :param verbose_flag: BOOL flag to determine logging level
     """
     # Logging
     setup_logging(verbose_flag)
