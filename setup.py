f4fc4136f12415ac57021c6d66b424d92ffcd50d
forestdussault <forest.dussault@inspection.gc.ca>
2018-04-23 14:23:16 -0400

    1.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="olcnastools",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(),
     author="Forest Dussault",
     author_email="forest.dussault@inspection.gc.ca",
