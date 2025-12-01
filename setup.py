0a25fdf58e9fc02a2b07bb2293454a449646f172
Adam Koziol <adam.koziol@inspection.gc.ca>
2025-12-01 20:54:53 +0000

    fix: setup must have entry_point rather than scripts

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="olcnastools",
-    version="1.2.6",
+    version="1.2.7",
     packages=find_packages(),
     author="Adam Koziol, Andrew Low, Forest Dussault",
     author_email="adam.koziol@inspection.gc.ca",
     url="https://github.com/OLC-Bioinformatics/OLC_NAS_Tools",
-    scripts={
+    entry_points={
         'console_scripts': [
             'nastools.py = nastools.olcnastools:main'
         ]
