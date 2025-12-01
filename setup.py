75084be99b53021b60ef28702b157db1f3c58ec0
Adam Koziol <adam.koziol@inspection.gc.ca>
2025-12-01 16:39:28 +0000

    chore: update setup.py and create pyproject.toml for PIP package creation

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="olcnastools",
-    version="1.1.9",
+    version="1.2.0",
     packages=find_packages(),
     author="Adam Koziol, Andrew Low, Forest Dussault",
     author_email="adam.koziol@inspection.gc.ca",
     url="https://github.com/OLC-Bioinformatics/OLC_NAS_Tools",
     scripts=['nastools/nastools.py'],
-    install_requires=['olctools']
 )
