5c90f63c58bda5d3ba8bbcc28dc5d65a9677e61d
Adam Koziol <adam.koziol@inspection.gc.ca>
2025-12-01 19:08:27 +0000

    fix: remove pyproject.toml to keep a single file responsible for setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="olcnastools",
-    version="1.2.1",
+    version="1.2.2",
     packages=find_packages(),
     author="Adam Koziol, Andrew Low, Forest Dussault",
     author_email="adam.koziol@inspection.gc.ca",
