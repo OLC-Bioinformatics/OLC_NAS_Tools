c748f33a9d084d612bb83f35bdcb405e9fd7b44c
Adam Koziol <adam.koziol@inspection.gc.ca>
2025-12-01 16:29:12 +0000

    fix: remove dependency on OLCTools package

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="olcnastools",
-    version="1.1.8",
+    version="1.1.9",
     packages=find_packages(),
     author="Adam Koziol, Andrew Low, Forest Dussault",
-    author_email="adam.koziol@canada.ca",
-    url="https://github.com/OLC-Bioinformatics/OLC_NAS_Tools",  # link to the repo
+    author_email="adam.koziol@inspection.gc.ca",
+    url="https://github.com/OLC-Bioinformatics/OLC_NAS_Tools",
     scripts=['nastools/nastools.py'],
     install_requires=['olctools']
 )
