3bbf84a785ff2a1de94fcaf99ed1bb4b4ebe8f73
Adam Koziol <adam.koziol@inspection.gc.ca>
2025-12-01 20:18:55 +0000

    fix: set console script to be nastools.py not nastools

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="olcnastools",
-    version="1.2.3",
+    version="1.2.5",
     packages=find_packages(),
     author="Adam Koziol, Andrew Low, Forest Dussault",
     author_email="adam.koziol@inspection.gc.ca",
     url="https://github.com/OLC-Bioinformatics/OLC_NAS_Tools",
     entry_points={
         'console_scripts': [
-            'nastools = nastools.nastools:main'
+            'nastools.py = nastools.nastools:main'
         ]
     },
 )
