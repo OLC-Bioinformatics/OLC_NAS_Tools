38dbfa82db0560b6debc5d6caa70d1734fc6a55c
Adam Koziol <adam.koziol@inspection.gc.ca>
2025-12-01 19:02:11 +0000

    fix: address issue with publish.yml:  Unrecognized named-value: 'secrets'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="olcnastools",
-    version="1.2.0",
+    version="1.2.1",
     packages=find_packages(),
     author="Adam Koziol, Andrew Low, Forest Dussault",
     author_email="adam.koziol@inspection.gc.ca",
